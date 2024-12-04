267faf8ad793b8ad1dda62d11a166678416dc7da
KevinMontecastelo <Kevin.r.nunez@gmail.com>
2024-12-04 14:22:18 +0100

    Componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TresRayaComponent } from './tres-raya/tres-raya.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TresRayaComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule,
