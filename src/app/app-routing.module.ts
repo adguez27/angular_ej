b9184c593436740dda19e66591d0c7efb6a945fb
Alex Domínguez <doalex7@gmail.com>
2024-12-05 09:26:06 +0100

    Añadida navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalcComponent } from './calc/calc.component';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'calc', component: CalcComponent },
+  { path: 'tictactoe', component: TictactoeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
